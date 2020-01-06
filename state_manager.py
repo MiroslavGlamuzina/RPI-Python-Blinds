16fcfdc912ff6bd372fc9a40d633a8163d9489b6
MiroslavGlamuzina <miro.glamuzina@gmail.com>
2020-01-06 18:34:33 +0000

    added files

---
+++ state_manager.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+
+MAX_UP = 6000
+MIN_DOWN = 0
+STEPS = 2048
+
+
+def calibrate():
+    file = open('/home/pi/pyBlinds/state.txt', 'r+')
+    file.seek(0)
+    file.truncate()
+
+    file.write("0")
+    file.close()
+
+
+def get_state(file):
+    cur = file.readline()
+    cur = int(0 if cur == '' else cur)
+
+    #  Clean the file - TODO Check that cur is set properly .
+    file.seek(0)
+    file.truncate()
+
+    return cur
+
+
+def get_next_values(current_value, action):
+    steps = STEPS
+    if action == 'UP':
+        next_value = int(current_value + STEPS)
+        if next_value >= MAX_UP:
+            next_value = MAX_UP
+            steps = MAX_UP - current_value
+    else:
+        next_value = int(current_value - STEPS)
+        if next_value <= MIN_DOWN:
+            next_value = MIN_DOWN
+            steps = current_value
+
+    return [next_value, steps]
+
+
+def update(action):
+    file = open('/home/pi/pyBlinds/state.txt', 'r+')
+    cur = get_state(file)
+    tmp = get_next_values(cur, action)
+    next_value = tmp[0]
+    next_steps = tmp[1]
+
+    log(
+        "Previous value: " + str(cur) + "\r\n" +
+        "Value: " + str(next_value) + "\r\n" +
+        "Steps: " + str(next_steps) + "\r\n"
+    )
+
+    file.write(str(next_value))
+    file.close()
+    return next_steps
+
+
+def log(str_value):
+    file = open('/home/pi/pyBlinds/log.txt', 'a+')
+    file.write(str(datetime.now()) + "\r\n" + str_value + "\r\n\r\n")
+    print(str(datetime.now()) + "\r\n" + str_value + "\r\n\r\n")
+    file.close()
---
+++ stepper.py
@@ -0,0 +1,67 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+from state_manager import *
+
+#  SETUP PINS
+GPIO.setmode(GPIO.BCM)
+control_pins = [18, 23, 24, 17]
+for pin in control_pins:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, 0)
+
+# CONSTANTS
+DELAY = 0.00075
+UP_SEQUENCE = [
+    [1, 0, 0, 0],
+    [1, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 1, 0],
+    [0, 0, 1, 0],
+    [0, 0, 1, 1],
+    [0, 0, 0, 1],
+    [1, 0, 0, 1]
+]
+DOWN_SEQUENCE = [
+    [1, 0, 0, 1],
+    [0, 0, 0, 1],
+    [0, 0, 1, 1],
+    [0, 0, 1, 0],
+    [0, 1, 1, 0],
+    [0, 1, 0, 0],
+    [1, 1, 0, 0],
+    [1, 0, 0, 0],
+]
+
+
+def move(sequence, delay, steps):
+    for i in range(steps):
+        sys.stdout.write("\r Steps: " + str(i) + "/" + str(steps))
+        for halfstep in range(8):
+            for pin in range(4):
+                GPIO.output(control_pins[pin], sequence[halfstep][pin])
+            time.sleep(delay)
+    GPIO.cleanup()
+
+
+if sys.argv[1] == '1':
+    TYPE = DOWN_SEQUENCE
+    STEPS = update('DOWN')
+elif sys.argv[1] == '0':
+    TYPE = UP_SEQUENCE
+    STEPS = update('UP')
+elif sys.argv[1] == 'calibrate' or sys.argv[1] == 'c':
+    log("Calibrating...\r\nEnsure to kill when blinds are fully down (at 0)")
+    TYPE = DOWN_SEQUENCE
+    STEPS = 6000
+    calibrate()
+elif sys.argv[1] == 'help' or sys.argv[1] == 'h':
+    log("usage: python stepper.py [1(down)|0(up)|calibrate/c|help/h]")
+    exit()
+else:
+    log('TYPE was not defined')
+    exit()
+
+move(TYPE, DELAY, STEPS)
+
+print('Done!')
